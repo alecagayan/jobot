4a90132240ef02c2bd623ce41f8e32cc4d2f7a30
Ubuntu <ubuntu@ip-172-26-1-44.ec2.internal>
2021-11-23 20:30:13 +0000

    working tags

--- cogs/tags.py
+++ cogs/tags.py
@@ -5,49 +5,46 @@ import sqlite3
 class Tags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
+    #command that takes in a tag and searches for the tag in the Tag column of the sqlite3 database
+    #once the tag is found, the text in the corresponding Ref column is sent to the channel
     @commands.command()
     async def tag(self, ctx, *, tag):
+        conn = sqlite3.connect('./data/db/database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM tags WHERE Tag = ?", (tag,))
+        result = c.fetchone()
+        if result is None:
+            await ctx.send('Tag not found')
+        else:
+            await ctx.send(result[1])
+        conn.close()
 
-        db = sqlite3.connect("./data/db/database.db", check_same_thread=False)
-        cur = db.cursor()
-
-        if tag is not None:
-            cur.execute(f"SELECT Ref FROM Tags WHERE Tag = ?", (tag,)) 
-            result = cur.fetchone()
-
-        cur.close()
-        db.close()
-        await ctx.send(result)
-
+    #command that takes in a tag and a text and adds the tag to the Tag column of the sqlite3 database
+    #the text is added to the Ref column of the sqlite3 database
     @commands.command()
     async def addtag(self, ctx, *, tag):
+        conn = sqlite3.connect('./data/db/database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM tags WHERE Tag = ?", (tag,))
+        result = c.fetchone()
+        if result is None:
+            #add a new row with the tag and ask the user what they want the content to be
+            await ctx.send('What would you like the content to be?')
+            def check(m):
+                return m.author == ctx.author
+            msg = await self.bot.wait_for('message', check=check)
+            c.execute("INSERT INTO tags VALUES (?, ?)", (tag, msg.content))
+            conn.commit()
 
-        db = sqlite3.connect("./data/db/database.db", check_same_thread=False)
-        cur = db.cursor()
+            await ctx.send('Tag added')
+        else:
+            await ctx.send('Tag already exists')
+        conn.close()
 
-        if tag is not None:
-            print(tag)
-            cur.execute(f"SELECT Tag FROM Tags WHERE Tag = ?", (tag,))
-            result = cur.fetchone()
-            if result is None:
 
-                def check(m):
-                    return m.author == ctx.author
 
-                await ctx.send('What would you like the content of `' + tag + '` to be?')
-                ref = await self.bot.wait_for('message', check=check)
-                cur.execute("INSERT INTO Tags(Tag, Ref) VALUES(?,?)", (tag, ref))
 
-                db.commit()
-                cur.close()
-                db.close()
-            else:
-                await ctx.send("That tag already exists!")
-    
-
-        
-        
 
 def setup(bot):
     bot.add_cog(Tags(bot))
